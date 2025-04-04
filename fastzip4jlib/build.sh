4cada06676f83247b0d410bf3f574cdab3415059
Hoang Nguyen <hoangna1204@gmail.com>
2025-04-04 13:50:09 +0200

    fix: update build.sh

--- fastzip4jlib/build.sh
+++ fastzip4jlib/build.sh
@@ -1,8 +1,8 @@
 # Build for Linux
-GOOS=linux GOARCH=amd64 go build -buildmode=c-shared -o ../fastzip4j/src/main/resources/lib/libfastzip4j.so fastzip4j.go
+env GOOS=linux GOARCH=amd64 go build -buildmode=c-shared -o ../fastzip4j/src/main/resources/lib/libfastzip4j.so fastzip4j.go
 
 # Build for Windows
-GOOS=windows GOARCH=amd64 go build -buildmode=c-shared -o ../fastzip4j/src/main/resources/lib/libfastzip4j.dll fastzip4j.go
+env GOOS=windows GOARCH=amd64 go build -buildmode=c-shared -o ../fastzip4j/src/main/resources/lib/libfastzip4j.dll fastzip4j.go
 
 # Build for macOS
-GOOS=darwin GOARCH=amd64 go build -buildmode=c-shared -o ../fastzip4j/src/main/resources/lib/libfastzip4j.dylib fastzip4j.go
+env GOOS=darwin GOARCH=amd64 go build -buildmode=c-shared -o ../fastzip4j/src/main/resources/lib/libfastzip4j.dylib fastzip4j.go
